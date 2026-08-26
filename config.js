62c01fdec3629170beecc319ae7ad8dd8baddff1
Perfectsmilefr <perfect.smile.fr.ch@gmail.com>
2026-08-26 17:50:38 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://qxkszhwllffjeyuzmpuq.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4a3N6aHdsbGZmamV5dXptcHVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc1NjUyMTksImV4cCI6MjEwMzE0MTIxOX0.Mncnp-RfjklFkDak7Ud_TvCusOwNfbyLKiGY5ffPDZU",
+  FUNCTIONS_URL: "https://qxkszhwllffjeyuzmpuq.supabase.co/functions/v1",
+  STRIPE_PK: "pk_live_51TdU0OCo1vntdAa9fVjtPGJkaTM8BJCmDM77BaXfQ03fcht3hWkf5WnVwpFnTincUk4L7ZaZ2njiLxMw2ShfCkeY00GGpCXqIW"
+};
